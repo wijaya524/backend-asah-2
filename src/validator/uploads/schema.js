0b02b7345bc120bd0298ada2e665871dd42114ef
Aryansyah <wijaya524@users.noreply.github.com>
2025-10-21 11:26:36 +0700

    g

---
+++ src/validator/uploads/schema.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+const Joi = require('joi');
+
+const ImageHeadersSchema = Joi.object({
+  'content-type': Joi.string().valid('image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/webp').required(),
+}).unknown();
+
+module.exports = { ImageHeadersSchema };
